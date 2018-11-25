17ad21cc42cafc95ff2cb0e784db3743b68e0df7
aycagrlyk <gurleyikayca@gmail.com>
2018-11-25 23:41:12 +0300

    Add PuddleTile

---
+++ Server/src/main/java/net/simon987/server/game/world/TilePuddle.java
@@ -0,0 +1,10 @@
+package net.simon987.server.game.world;
+
+public class TilePuddle extends Tile {
+
+    public static final int ID = 5;
+    @Override
+    public int getId() {
+        return ID;
+    }
+}
