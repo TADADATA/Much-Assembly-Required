02714211ac72553936712f0343766d704896ce91
Aslı Ural <aasliuural@gmail.com>
2018-11-26 15:00:13 +0300

    I added TileShortCircuit

---
+++ Server/src/main/java/net/simon987/server/game/world/TileShortCircuit.java
@@ -0,0 +1,11 @@
+package net.simon987.server.game.world;
+
+public class TileShortCircuit extends Tile {
+
+    public static final int ID = 6;
+
+    @Override
+    public int getId() {
+        return ID;
+    }
+}
