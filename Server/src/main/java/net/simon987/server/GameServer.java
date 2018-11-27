0f987df6ab08fd228e23f79f44d9b4ec42bba62c
Deniz Beştepe <debestepe@gmail.com>
2018-11-27 19:25:59 +0300

    Magnetic tile added to the game registry.

--- Server/src/main/java/net/simon987/server/GameServer.java
+++ Server/src/main/java/net/simon987/server/GameServer.java
@@ -127,6 +127,7 @@ public class GameServer implements Runnable {
         gameRegistry.registerTile(TileWall.ID, TileWall.class);
         gameRegistry.registerTile(TileCopper.ID, TileCopper.class);
         gameRegistry.registerTile(TileIron.ID, TileIron.class);
+        gameRegistry.registerTile(TileMagnet.ID, TileMagnet.class);
     }
 
     public GameUniverse getGameUniverse() {
