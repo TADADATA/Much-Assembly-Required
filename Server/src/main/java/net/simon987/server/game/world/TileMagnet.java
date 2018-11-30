8193f83a97c0fdea82c10d3adf4c72b01034c43a
Deniz Beştepe <debestepe@gmail.com>
2018-11-30 18:11:07 +0300

    INFO_MAGNET added to World and tile id updated.

--- Server/src/main/java/net/simon987/server/game/world/TileMagnet.java
+++ Server/src/main/java/net/simon987/server/game/world/TileMagnet.java
@@ -2,7 +2,7 @@ package net.simon987.server.game.world;
 
 public class TileMagnet extends Tile {
 
-    public static final int ID = 4;//I'm not sure about this value
+    public static final int ID = 12;//I'm not sure about this value
 
     @Override
     public int getId() {
