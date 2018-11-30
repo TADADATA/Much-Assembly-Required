ae236f43cf68fb66cc42a3653f2e8b1ea64289e1
AsliUral <aasliuural@gmail.com>
2018-12-01 02:18:58 +0300

    I added tile config

--- Server/src/main/typescript/mar.ts
+++ Server/src/main/typescript/mar.ts
@@ -6,6 +6,8 @@ let RENDERER_HEIGHT = (window.innerHeight / 1.40) * window.devicePixelRatio;
 let DEBUG: boolean = true;
 
 let config = {
+    textShortCircuit: "#6242f4",
+    tileShortCircuitTint:0xFFFFFF,
     portalTint: 0xff43c8,
     tileTint: 0xFFFFFF,
     wallTint: 0xDDDDDD,
@@ -240,4 +242,3 @@ class Debug {
 DEBUG = false;
 
 let mar = new MarGame();
-
