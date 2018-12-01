a1cd1183ca97d4ac17ed1b860f5c3451c83a50c3
aycagrlyk <gurleyikayca@gmail.com>
2018-12-01 16:36:14 +0300

    isDead method added to Puddle Tile

--- Server/src/main/java/net/simon987/server/game/world/TilePuddle.java
+++ Server/src/main/java/net/simon987/server/game/world/TilePuddle.java
@@ -1,10 +1,19 @@
 package net.simon987.server.game.world;
-
+import net.simon987.server.game.objects.GameObject;
 public class TilePuddle extends Tile {
 
     public static final int ID = 5;
     @Override
     public int getId() {
         return ID;
+    }
+    
+    
+    public boolean isDead() { //not certain 
+        if(getMapInfo()==0x0250) {
+        	setDead(true);
+        	return true;
+        }
+        return false;
     }
 }
