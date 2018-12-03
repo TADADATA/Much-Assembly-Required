c3d1c89f461ac824fa5fe943128ce8cb37fa9ab6
akyurektuna <akyurekktuna@gmail.com>
2018-12-03 23:01:42 +0300

    changed tile id

--- Server/src/main/java/net/simon987/server/game/world/TileBiofood.java
+++ Server/src/main/java/net/simon987/server/game/world/TileBiofood.java
@@ -1,7 +1,7 @@
 package net.simon987.server.game.world;
 
 public class TileBiofood extends Tile{
-	public static final int ID = 8;
+	public static final int ID = 13;
 
 	@Override
 	public int getId(){
