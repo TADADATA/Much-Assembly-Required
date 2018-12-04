26f0ed5aa41ea2427cf08b870363c88ac332511b
aycagrlyk <gurleyikayca@gmail.com>
2018-12-04 03:44:42 +0300

    PuddleTile ID is changed

--- Server/src/main/java/net/simon987/server/game/world/TilePuddle.java
+++ Server/src/main/java/net/simon987/server/game/world/TilePuddle.java
@@ -2,7 +2,7 @@ package net.simon987.server.game.world;
 import net.simon987.server.game.objects.GameObject;
 public class TilePuddle extends Tile {
 
-    public static final int ID = 5;
+    public static final int ID = 14;
     @Override
     public int getId() {
         return ID;
