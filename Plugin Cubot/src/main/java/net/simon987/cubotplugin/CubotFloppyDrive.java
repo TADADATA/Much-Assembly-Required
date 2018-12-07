b1d77e9d7f2e0a41869801b507ff53a650f2672b
Deniz Beştepe <debestepe@gmail.com>
2018-12-07 20:10:11 +0300

    Added hash methods for demo.

--- Plugin Cubot/src/main/java/net/simon987/cubotplugin/CubotFloppyDrive.java
+++ Plugin Cubot/src/main/java/net/simon987/cubotplugin/CubotFloppyDrive.java
@@ -42,12 +42,14 @@ public class CubotFloppyDrive extends CubotHardwareModule {
 
 
         if(super.cubot.getWorld().getTileMap().getTileIdAt(super.cubot.getX(), super.cubot.getY()) == TileMagnet.ID){
-          System.out.println("on magnetic tile");
+          System.out.println("On magnetic tile. FloppyDisk memory hash before process: "+floppyDisk.getMemoryHash());
           if(floppyDisk != null){
             floppyDisk.corruptFloppyDisk(corruptionBlockSize);
+            System.out.println("On magnetic tile. FloppyDisk memory hash after process: "+floppyDisk.getMemoryHash());
           }
         }else{
-          System.out.println("not on magnetic tile");
+          System.out.println("On magnetic tile. FloppyDisk memory hash before process: "+floppyDisk.getMemoryHash());
+          System.out.println("On magnetic tile. FloppyDisk memory hash after process: "+floppyDisk.getMemoryHash());
         }
 
         int a = getCpu().getRegisterSet().getRegister("A").getValue();
